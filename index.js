cbed2429037b5d12c5b54ce8bda34e139534d006
120356aa <yurlovandrew@gmail.com>
2020-02-11 23:55:14 -0800

    server completed

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const PORT = process.env.PORT;
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const { Lead } = require('./templates/leads.js');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.post('/api/send', (req, res) => {
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'stuntlyfee@gmail.com',
+            pass: 'H@cked123'
+        }
+    });
+
+    let mailOptions = {
+        from: '"Sam" <sam@gromarketing.io>',
+        // to: ['yurlovandrew@gmail.com'],
+        to: ['yurlovandrew@gmail.com'],
+        subject: 'RealEstate Form Leads',
+        html: Lead(req.body),
+    };    
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) return console.log(err)
+        else return info.messageId
+    });
+});
+
+app.listen(PORT || 5000, () => {
+    console.log("Listening on port ", PORT)
+});
---
+++ templates/leads.js
@@ -0,0 +1,22 @@
+const Lead = data => {
+    return `
+        <h2 style="color: #E05B50">New Real Estate Form Lead</h2>
+        <br />
+
+        <span style="font-weight: bold;">Type of Property: </span>Type of Property: ${data.type} <br />
+        <span style="font-weight: bold;">Type of Property: </span>Condition of Property: ${data.condition} <br />
+        <span style="font-weight: bold;">Value of Property: </span>${data.value} <br />
+        <span style="font-weight: bold;">Sale Timeline: </span>${data.timeline} <br />
+        <br />
+
+        <span style="font-weight: bold;">Address: </span>${data.address} <br />
+        <span style="font-weight: bold;">Zip Code: </span>${data.zip} <br />
+        <br />
+
+        <span style="font-weight: bold;">Name: </span>${data.name} <br />
+        <span style="font-weight: bold;">Phone: </span>${data.phone} <br />
+        <span style="font-weight: bold;">Email: </span>${data.email} <br />
+    `;
+  };
+  
+  module.exports = { Lead };
